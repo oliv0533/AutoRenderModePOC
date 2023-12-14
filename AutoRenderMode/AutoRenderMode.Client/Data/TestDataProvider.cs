e8ca57dd9e9ff04f66677d449f5e794acc714fe0
oliv0533 <oliverchristensen93@gmail.com>
2023-12-14 21:55:30 +0100

    Add project files.

---
+++ AutoRenderMode/AutoRenderMode.Client/Data/TestDataProvider.cs
@@ -0,0 +1,29 @@
+﻿using AutoRenderMode.Client.Models;
+using Bogus;
+
+namespace AutoRenderMode.Client.Data;
+
+public static class TestDataProvider
+{
+    public static List<TestModel> GetTestData()
+    {
+        var testData = new List<TestModel>();
+
+        Thread.Sleep(100);
+
+        var faker = new Faker();
+
+        for (int i = 0; i < 500; i++)
+        {
+            testData.Add(new TestModel
+            {
+                Name = faker.Name.FullName(),
+                Description = faker.Lorem.Sentence(),
+                Type = faker.PickRandom<TestModelType>().ToString(),
+                Value = faker.Random.Int(0, 100)
+            });
+        }
+
+        return testData;
+    }
+}
---
+++ AutoRenderMode/AutoRenderMode.Client/Models/TestModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AutoRenderMode.Client.Models;
+
+public class TestModel
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Type { get; set; }
+    public int Value { get; set; }
+}
+
+public enum TestModelType
+{
+    Foo,
+    Bar,
+    Baz
+}
